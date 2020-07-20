3d26fb25d49cfb1aa2cecdbd5e12c92cdbab79ca
Sharanesh R <ee19b116@smail.iitm.ac.in>
2020-07-20 23:59:57 +0530

    Update making_star.py

--- making_star.py
+++ making_star.py
@@ -19,9 +19,13 @@ def func():
     #rospy.loginfo("X:axis at %f \n",turtle_x)
     rospy.wait_for_service("/turtle1/teleport_absolute")
     tele_abs = rospy.ServiceProxy("/turtle1/teleport_absolute", TeleportAbsolute)
+    start_coord = int(input("Enter the start point (symmetric coordinate; enter only one value): "))
+    num_shape = int(input("Enter the number of point edged star the bot should cover: "))
+    angle =  (3.14/num_shape)
+                          
     try:
       #resp1 = tele_abs(int(turtle_x),int(turtle_y),0)
-      resp2 = tele_abs(3,3,0)
+      resp2 = tele_abs(start_coord,start_coord,0)
     except rospy.ServiceException as exc:
       print("Service did not process request: " + str(exc))
     
@@ -29,20 +33,21 @@ def func():
     clear = rospy.ServiceProxy("/clear",Empty)
     res = clear()
 
-
     rospy.wait_for_service("/turtle1/teleport_relative")
     tele_rel = rospy.ServiceProxy("/turtle1/teleport_relative",TeleportRelative)
+   
     try:
-      res1 = tele_rel(2,0.63)
-      res2 = tele_rel(2,5.025)
-      res3 = tele_rel(2,2.513)
-      res4 = tele_rel(2,5.025)
-      res5 = tele_rel(2,2.513)
-      res6 = tele_rel(2,5.025)
-      res7 = tele_rel(2,2.513)
-      res8 = tele_rel(2,5.025)
-      res9 = tele_rel(2,2.513)
-      res10 = tele_rel(2,5.025)
+      tele_rel(2,angle)
+      tele_rel(2,angle*4)
+      tele_rel(2,angle*2)
+      tele_rel(2,angle*4)
+      tele_rel(2,angle*2)
+      tele_rel(2,angle*4)
+      tele_rel(2,angle*2)
+      tele_rel(2,angle*4)
+      tele_rel(2,angle*2)
+      tele_rel(2,angle*4)
+      
     except:
       print("Service did not process request: " + str(exc))
   
